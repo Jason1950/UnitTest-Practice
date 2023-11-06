07988620e38d85b6feec350fb4d3434e8065330e
henry.shen <henry.shen@onelab.tw>
2023-11-06 12:14:04 +0800

    feat : add joey code

---
+++ Coach91/Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Coach91.Controllers
+{
+    /// <summary>
+    /// Login: ONEbook.Member.Services.Impl.ProcessLoginService.Login/ Initialization
+    /// QRLogin: ONEbook.Member.Services.Impl.ProcessLoginService.Login/ Initialization
+    /// Initialize: ONEbook.Member.Services.Impl.ProcessLoginService.Initialization
+    /// Logout: ONEbook.Member.Services.Impl.ProcessLoginService.Logout
+    /// Checkin: ONEbook.Member.Services.Impl.ProcessLoginService.LoginCheckin
+    /// GetESportsWebUrl: ONEbook.Member.Services.Impl.ProcessLoginService.GetESportsWebUrl
+    /// ExchangeToken: token service /at/create
+    /// DcsSettings: dcs, siteconfig
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        private DateTime _today;
+        public TestController()
+        {
+        }
+        
+        [HttpGet("CheckTodayIsMerryChristmas")]
+        public string CheckTodayIsMerryChristmas()
+        {
+            var today = GetNow();
+            if (today.Month == 12 && today.Day == 25)
+            {
+                return "Merry Xmas";
+            }
+            return "Today is not Xmas";
+        }
+        
+        public virtual DateTime GetNow()
+        {
+            return DateTime.Now;
+        }
+        
+    }
+    
+}
---
+++ TestProject1/JoeyTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestProject1;
+
+public class Period
+{
+    public Period(DateTime start, DateTime end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public DateTime Start { get; private set; }
+    public DateTime End { get; private set; }
+
+    public int IntervalDays()
+    {
+        var intervalDays = (End - Start).Days + 1;
+        return intervalDays;
+    }
+}
+
+public class ValidationResult
+{
+    public ValidationResult(string errorMessage, bool isValid)
+    {
+        ErrorMessage = errorMessage;
+        IsValid = isValid;
+    }
+
+    public string ErrorMessage { get; private set; }
+    public bool IsValid { get; private set; }
+
+    public void DumpLog()
+    {
+        if (!IsValid)
+        {
+            Console.WriteLine($"isValid:{IsValid}, errorMessage:{ErrorMessage}");
+        }
+    }
+}
+
+[TestFixture]
+public class JoeyTests
+{
+    [Test]
+    public void interval_days()
+    {
+        var start = new DateTime(2000, 10, 30);
+        var end = new DateTime(2000, 11, 2);
+        var intervalDays = (end - start).Days + 1;
+        Assert.AreEqual(4, intervalDays);
+    }
+
+    [Test]
+    public void out_refactor_sample()
+    {
+        var product = new Product() { Price = -1 };
+        var validationResult = product.Validate();
+
+
+        validationResult.DumpLog();
+    }
+
+}
+
+public class Product
+{
+    public int Price { get; set; }
+
+    public ValidationResult Validate()
+    {
+        var isValid = true;
+        var errorMessage = "";
+        if (Price < 0)
+        {
+            isValid = false;
+            errorMessage = "price should > 0";
+        }
+
+        return new ValidationResult(errorMessage, isValid);
+    }
+}
